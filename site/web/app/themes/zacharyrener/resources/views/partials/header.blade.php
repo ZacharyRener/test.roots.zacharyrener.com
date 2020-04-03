ceb287268a6a7bd8f9f591779a2ffd54fd2a6b32
Zachary Rener <me@zacharyrener.com>
2020-04-02 22:53:48 -0500

    added front page carousel. not complete

---
+++ site/web/app/themes/zacharyrener/resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class='container'>
+        <div class='site-logo'>
+            Zachary Rener
+            <div class='site-excerpt'>WordPress Developer</div>
+        </div>
+        
+        <div class='site-menu'>
+            <a href=''>All Projects</a><a href=''>GitHub</a>
+        </div>
+    </div>
+</header>
---
+++ site/web/app/themes/zacharyrener/resources/views/partials/project-carousel.blade.php
@@ -0,0 +1,19 @@
+<div id="front-page-carousel" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#front-page-carousel" data-slide-to="0" class="active"></li>
+    <li data-target="#front-page-carousel" data-slide-to="1"></li>
+    <li data-target="#front-page-carousel" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class='blue-overlay'></div>
+      <video  loop="true" autoplay="autoplay" controls="controls" id="vid" muted>
+        <source src='/app/uploads/2020/04/SHR-Site-Take-Six.mp4' type="video/mp4">
+      </video>
+      <div class='text-section'>
+        <h2>SHR <br/>Consulting</h2>
+        <p>WordPress Theme from scratch</p>
+      </div>
+    </div>
+  </div>
+</div>
